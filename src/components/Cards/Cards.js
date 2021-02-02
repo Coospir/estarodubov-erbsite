e01189c70a3085955b3f2f9ef36f1eacdb35a911
Coospir <eugene.starodubov@gmail.com>
2021-02-02 18:22:02 +0300

    Updated skills cards

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -9,13 +9,13 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem src="https://miro.medium.com/max/1200/1*aK6-A2EmtFCYriaKgoguPw.png" text="HTML/CSS" label='Skills' path='/about-me'/>
-                        <CardItem src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png" text="Javascript" label='Skills' path='/about-me'/>
+                        <CardItem src="https://miro.medium.com/max/1200/1*aK6-A2EmtFCYriaKgoguPw.png" text="HTML/CSS: for making cool and adaptive websites" label='Skills' path='/about-me'/>
+                        <CardItem src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/480px-JavaScript-logo.png" text="Javascript: to make website's life" label='Skills' path='/about-me'/>
                     </ul>
                     <ul className='cards__items'>
-                        <CardItem src="https://itsg-global.com/wp-content/uploads/2016/09/react-js-to-use-or-not-to-use.png" text="React JS" label='Skills' path='/about-me'/>
-                        <CardItem src="https://www.surrealcms.com/uploads/nodejs-logo.png" text="Node JS" label='Skills' path='/about-me'/>
-                        <CardItem src="https://nemcd.com/wp-content/uploads/2019/08/php-slon.png" text="PHP 5.6+" label='Skills' path='/about-me'/>
+                        <CardItem src="https://itsg-global.com/wp-content/uploads/2016/09/react-js-to-use-or-not-to-use.png" text="React JS: level up of creating web apps" label='Skills' path='/about-me'/>
+                        <CardItem src="https://www.surrealcms.com/uploads/nodejs-logo.png" text="Node JS: for getting data from the server" label='Skill ' path='/about-me'/>
+                        <CardItem src="https://nemcd.com/wp-content/uploads/2019/08/php-slon.png" text="PHP 5.6+: for getting data from the server" label='Skills' path='/about-me'/>
                     </ul>
                 </div>
             </div>
